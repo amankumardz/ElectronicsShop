a1cda5df0e9cb55ffc5768ac7da2cf8dc4f9b796
Aman Kumar <aman.kumar@intelegencia.com>
2026-04-24 14:29:26 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics;
+using ElectronicsShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElectronicsShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
